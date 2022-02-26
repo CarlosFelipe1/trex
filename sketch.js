eacb679ecb60dc261de8653b4266c316232d4258
CarlosFelipe1 <98185892+CarlosFelipe1@users.noreply.github.com>
2022-02-26 19:05:54 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -51,7 +51,7 @@ function setup(){
   grupoNuvens = new Group();
   grupoCactos = new Group();
 
-  trex.debug = true
+  trex.debug = false
   trex.setCollider("circle", 0, 0, 40)
   //trex.setCollider("rectangle", 60, 0, 100, 250, 90)
 
